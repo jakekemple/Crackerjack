4274ccebbb41fa616ef3af020ed88a5c2c5f53d0
Jake Kemple <jakek927@gmail.com>
2016-10-31 08:22:29 -0500

    comments & spacing for clarity

--- crackerjack.py
+++ crackerjack.py
@@ -8,10 +8,9 @@ import multiprocessing as mp
 import functools
 import itertools
 
-
-
+# Basic bruteforce algorithm. Tests combination possibilities
+# of characters in charset
 def bruteforce(charset, maxlength):
-
     return (
         list(''.join(candidate)
             for candidate in itertools.chain.from_iterable(
@@ -21,37 +20,27 @@ def bruteforce(charset, maxlength):
         )
     )
 
-
 def main(results, partial_charset):
-
     results = bruteforce(partial_charset, maxlength)
     return results
-
 
 if __name__ == "__main__":
     print("")
     print("Cores on machine to be used:", mp.cpu_count())
+
     cores_used = mp.cpu_count()
     charset = str(sys.argv[1])
     maxlength = int(sys.argv[2])
+
     start = time.time()
 
-    #print("Total simulations to do:", num_simulations, "/n")
-    #leftover = num_simulations % cores_used
-    #num_simulations = num_simulations // cores_used
-
-    #print("The cores will each do", num_simulations, "simulation, and")
-    #last_core_simulations = leftover + num_simulations
-    #print("the last core will do", last_core_simulations, "simulations")
-
-
+    charset_final = []
     results = []
     partial_func = functools.partial(main, results)
 
-    charset_final = []
-
     print("character set entered: ", charset)
 
+    # This code block creates partial_charsets from charset to be used in each process
     chunk_size = len(charset)//cores_used
     print("Chunk Size: ", chunk_size)
     if len(charset) % cores_used: chunk_size += 1
@@ -63,16 +52,16 @@ if __name__ == "__main__":
             except StopIteration: break
         charset_slice = ''.join(charset_slice)
         charset_final.append(charset_slice)
-
     charset_list = charset_final
 
-
+    # Creates list of partial_charsets to be iterated over by p.map
     partial_charsets = []
     for i in charset_list:
         partial_charsets.append(i)
 
     print("List of partial character sets to be used for possibilities list: ", partial_charsets)
 
+    # Multiprocessing setup here
     p = mp.Pool(cores_used)
     results = p.map(partial_func, partial_charsets)
     results = list(itertools.chain.from_iterable(results))
