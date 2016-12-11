6e7f4fccbef9683f6db07b8b39be7d6920af6c8a
Jake Kemple <jakek927@gmail.com>
2016-12-11 02:14:38 -0600

    Updated bruteforce algorithm to more efficiently generate charset combinations. Comments added and various refactoring

--- crackerjack.py
+++ crackerjack.py
@@ -2,72 +2,111 @@
 # Info: https://github.com/jakekemple/Crackerjack
 
 import sys
-import random
 import time
 import multiprocessing as mp
-import functools
-import itertools
+from itertools import product
 
-# Basic bruteforce algorithm. Tests combination possibilities
+# generator is ran by the individual processes
+def generator(first_char, wordtail_length, length):
+    # Generate all strings of length `length` starting with `first_char`.
+    # Once length > wordtail_length, only the wordtail_length characters
+    # need to be generated.
+    total_completed = 0
+    if length <= wordtail_length:
+        assert first_char == ""
+        # creating all combinations of length charset here (short strings)
+        for t in product(charset, repeat=length):
+            result = "".join(t)
+            #print(result)
+            # To compare to hash or keyword, do something with result here
+            total_completed += 1
+    else:
+        assert len(first_char) + wordtail_length == length
+        # creating all combinations of length charset here (longer strings)
+        for t in product(charset, repeat=wordtail_length):
+            result = first_char + "".join(t)
+            #print(result)
+            # To compare to hash or keyword, do something with result here
+            total_completed += 1 # word counter
+    return total_completed
+
+def count_completed(completed_chunk):
+    global total_completed
+    total_completed += completed_chunk
+    #print(total_completed, 'completed')
+
+# Bruteforce algorithm. Tests combination possibilities
 # of characters in charset
-def bruteforce(charset, maxlength):
-    return (
-        list(''.join(candidate)
-            for candidate in itertools.chain.from_iterable(
-            itertools.product(charset, repeat=i)
-                for i in range(1, maxlength + 1)
-            )
-        )
-    )
+def bruteforce(pool, chunk_size=1000000):
+    charset_length = len(charset)
 
-def main(results, partial_charset):
-    results = bruteforce(partial_charset, maxlength)
-    return results
+    # This chunk sets max largest wordtail_length to be
+    # computed without separating jobs by first_char
+    wordtail_length = 1
+    while charset_length**wordtail_length <= chunk_size:
+        wordtail_length += 1
+    wordtail_length -= 1
+
+    # short strings: generate words from size 1 to short words max
+    max_short_len = min(wordtail_length, maxlength)
+    for length in range(1, max_short_len + 1):
+        pool.apply_async(generator, args=("", wordtail_length, length),
+                         callback=count_completed)
+    # longer strings: generate words from short words max to maxlength
+    for length in range(max_short_len + 1, maxlength + 1):
+        # Get the cartesian product chars in the charset
+        for t in product(charset, repeat=length-wordtail_length):
+            first_char = "".join(t) # Remove punctuation
+
+            # Run generator function for on each process with args
+            pool.apply_async(generator, args=(first_char, wordtail_length, length),
+                             callback=count_completed)
+
 
 if __name__ == "__main__":
-    print("")
-    print("Cores on machine to be used:", mp.cpu_count())
 
-    cores_used = mp.cpu_count()
-    charset = str(sys.argv[1])
+    print("\nCores on machine to be used:", mp.cpu_count())
+
+    cores_used = None # defaults to all available cores
+    charset = int(sys.argv[1])
     maxlength = int(sys.argv[2])
+
+    total_completed = 0
+
+    if charset == 1:
+        charset = "0123456789"
+    elif charset == 2:
+        charset = "0123456789abcdefghijklmnopqrstuvwxyx"
+    elif charset == 3:
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyx"
+    elif charset == 4:
+        charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyx"
+    elif charset == 5:
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyx!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    elif charset == 6:
+        charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyx!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    else:
+        print("Please choose and integer between 1 and 6")
+        quit()
+
+    print("Character set chosen:", charset)
+    print("...Working....\n")
 
     start = time.time()
 
-    charset_final = []
-    results = []
-    partial_func = functools.partial(main, results)
-
-    print("character set entered: ", charset)
-
-    # This code block creates partial_charsets from charset to be used in each process
-    chunk_size = len(charset)//cores_used
-    print("Chunk Size: ", chunk_size)
-    if len(charset) % cores_used: chunk_size += 1
-    iterator = iter(charset)
-    for _ in range(cores_used):
-        charset_slice = list()
-        for _ in range(chunk_size):
-            try: charset_slice.append(next(iterator))
-            except StopIteration: break
-        charset_slice = ''.join(charset_slice)
-        charset_final.append(charset_slice)
-    charset_list = charset_final
-
-    # Creates list of partial_charsets to be iterated over by p.map
-    partial_charsets = []
-    for i in charset_list:
-        partial_charsets.append(i)
-
-    print("List of partial character sets to be used for possibilities list: ", partial_charsets)
-
     # Multiprocessing setup here
-    p = mp.Pool(cores_used)
-    results = p.map(partial_func, partial_charsets)
-    results = list(itertools.chain.from_iterable(results))
+    pool = mp.Pool(cores_used)
+    bruteforce(pool)
+    pool.close()
+    pool.join()
 
-    #print(results)
-    print("Final List: ", results)
+    # Add the size of each charset^i, where each i is 1 through maxlength
+    total_possibility_size = sum(len(charset)**i
+                   for i in range(1, maxlength + 1))
+
+    # Check that the number of words generated match the expected total number of all possibilities
+    assert total_completed == total_possibility_size, (total_completed, total_possibility_size)
+
 
     elapsed_time = time.time() - start
-    print("Elapsed time: {}".format(elapsed_time))
+    print("Elapsed time: {}".format(elapsed_time), "\n")
